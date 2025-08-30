a43b710e2932e12b65ab9bec7cafd81bc5e2937c
Jinyao Zhang DeSandies <jzd@Jinyaos-MacBook-Pro.local>
2025-08-30 14:25:32 -0700

    final submission

---
+++ CSCI 51/Assignment1/Assignment1.py
@@ -0,0 +1,9 @@
+name= 'jinyao'
+age = int(47//2.6)
+print(f'my name is {name}, I am {age} years old')
+#I hope that using an f string adds a slight, interesting twist to the task for both me and the grader
+
+for i in range(47):
+    print('I love CSCI 051')
+#can I use pycharm on my Mac or is VSC a hard requirement?
+#can I make a public github profile with a respitory of all of my responses? I generally won't push onto it until after the due dates if that helps
