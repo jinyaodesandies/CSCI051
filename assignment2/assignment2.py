574f4ec673740436691f63a1f31929392d6705e5
Jinyao Zhang DeSandies <jzdj2025@cs229-mac05.campus.pomona.edu>
2025-09-05 13:42:50 -0700

    assignment 2 added

---
+++ assignment2/assignment2.py
@@ -0,0 +1,27 @@
+# Assignment 2: Are You an Introvert?
+
+# Name: TODO: Replace with your name
+
+print("Answer the following questions with 'yes' or 'no':")
+
+# TODO: Prompt the user to answer the following 4 yes/no questions:
+# 1. Do you prefer spending time alone?
+# 2. Do you find large gatherings draining?
+# 3. Do you enjoy deep one-on-one conversations over small talk?
+# 4. Do you often reflect before speaking or acting?
+# Rather than using the print function, we can combine
+# prompting the user and reading their input using the following syntax
+# answer = input("your prompt goes here")
+# You should have four different variables to store the
+# four different responses corresponding to the four different prompts
+
+# TODO Create four different bool variables, A-D, that will be True if the answer was "yes" and False otherwise.
+
+# TODO: Replace this with the simplified Boolean expression that you derived using the K-map
+introvert = ...  # ← your Boolean logic here
+
+
+if introvert:
+    print("You are likely an introvert")
+else:
+    print("You are likely an extrovert")
